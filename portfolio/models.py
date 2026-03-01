e24e78caba89542aecc6e88ed0806b4fd0855a08
Mamurjon <aliev.mamurjon27@gmail.com>
2026-03-01 12:58:20 -0500

    fix: fixed bugs

--- portfolio/models.py
+++ portfolio/models.py
@@ -24,3 +24,12 @@ class profile(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
