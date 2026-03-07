8d6b6b43fb5e74aa2a51425c556a1d1c4b8e2e59
Mamurjon <aliev.mamurjon27@gmail.com>
2026-03-07 13:05:42 -0500

    ch

--- portfolio/models.py
+++ portfolio/models.py
@@ -20,7 +20,7 @@ class profile(models.Model):
     loction = models.CharField(max_length=30)
     email = models.EmailField(blank=True, null=True)
     github_link =models.CharField(max_length=50)
-    linkedin = models.CharField(max_length=50)
+    linkedin = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
